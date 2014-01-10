40679681f1e3e6dad987fb56dd30c077dd9d6742
Jahfer Husain <jahferh@gmail.com>
2014-01-10 00:18:55 -0500

    Basic implementation of operational transform

--- src/ot/core.clj
+++ src/ot/core.clj
@@ -9,42 +9,55 @@
 (defn op [type val]
   {:type type :val val})
 
-;; (defn incl-trans [ins1 ins2]
-;;   (let [p1 (:pos ins1) p2 (:pos ins2)
-;;         sid1 (:owner ins1) sid2 (:owner ins2)]
-;;     (cond
-;;       (< p1 p2) ins1
-;;       (and (= p1 p2) (< sid1 sid2)) ins1
-;;       :else (update-in ins1 [:pos] inc))))
+(defn ot [ops1 ops2 ops1' ops2']
+  (if (or (seq ops1) (seq ops2))
+    (let [op1 (first ops1)
+          op2 (first ops2)
+          type1 (:type op1)
+          type2 (:type op2)]
+      (cond
+       (= :ins type1)
+         (recur (rest ops1) ops2 (conj ops1' op1) (conj ops2' (op :ret 1)))
+       (= :ins type2)
+         (recur ops1 (rest ops2) (conj ops1' (op :ret 1)) (conj ops2' op2))
 
-;; (defn excl-trans [ins1 ins2]
-;;   (let [p1 (:pos ins1) p2 (:pos ins2)
-;;         sid1 (:owner ins1) sid2 (:owner ins2)]
-;;     (cond
-;;       (< p1 p2) ins1
-;;       (and (= p1 p2) (< sid1 sid2)) ins1
-;;       :else (update-in ins1 [:pos] dec))))
+       (= :ret type1 type2)
+         (let [v1 (:val op1)
+               v2 (:val op2)]
+           (cond
+            (> v1 v2)
+              (let [ops1 (conj (rest ops1) (assoc-in (first ops1) [:val] (- v1 v2)))
+                    ops2 (rest ops2)
+                    ret (op :ret v2)]
+                (recur ops1 ops2 (conj ops1' ret) (conj ops2' ret)))
+            (= op1 op2)
+              (let [ops1 (rest ops1)
+                    ops2 (rest ops2)
+                    ret (op :ret v2)]
+                (recur ops1 ops2 (conj ops1' ret) (conj ops2' ret)))
+            :else
+              (let [ops1 (rest ops1)
+                    ops2 (conj (rest ops2) (assoc-in (first ops2) [:val] (- v2 v1)))
+                    ret (op :ret v1)]
+                (recur ops1 ops2 (conj ops1' ret) (conj ops2' ret)))))))
 
+    [ops1' ops2']))
 
-;; (defn ot [op1 op2]
-;;   [[(op :ret 1) (op :ins "t") (op :ret 1)]
-;;    [(op :ins "a") (op :ret 1) (op :ret 1)]])
-
-(defn ot [op1 op2]
-  )
-
-(defn trans [opList1 opList2]
-  (ot opList1 opList2))
+(defn transform [ops1 ops2]
+  (ot ops1 ops2 (double-list) (double-list)))
 
 ;; Testing
 
 (def document "go")
 
 (def op-tom
-  ['(op :ret 2) '(op :ins "a")])
+  (double-list (op :ret 2) (op :ins "a")))
 (def op-jerry
-  ['(op :ret 2) '(op :ins "t")])
+  (double-list (op :ret 2) (op :ins "t")))
 
+(def tom-document
+  (str document "a"))
+(def jerry-document
+  (str document "t"))
 
-;; can't do this..have to pass individ ops to OT
-(trans op-tom op-jerry)
+(let [[a' b'] (transform op-tom op-jerry)])
