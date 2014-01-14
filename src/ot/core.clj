a66d6808ab37b1ad3f95d12521f75d50583dd024
Jahfer Husain <jahferh@gmail.com>
2014-01-13 22:06:46 -0500

    More code cleanup

--- src/ot/core.clj
+++ src/ot/core.clj
@@ -33,33 +33,30 @@
 
 (defn ot [ops1 ops2 ops1' ops2']
   (if (or (seq ops1) (seq ops2))
-    (let [op1 (first ops1)
-          op2 (first ops2)
-          type1 (:type op1)
-          type2 (:type op2)]
-      (cond
+    (cond
+     (insert? ops1)
+     (let [[ops1' ops2'] (assoc-op insert [ops1' ops2'] (first ops1))]
+       (recur (rest ops1) ops2 ops1' ops2'))
 
-       (insert? ops1)
-         (let [[ops1' ops2'] (assoc-op insert [ops1' ops2'] op1)]
-           (recur (rest ops1) ops2 ops1' ops2'))
-       (insert? ops2)
-         (let [[ops2' ops1'] (assoc-op insert [ops2' ops1'] op2)]
-           (recur ops1 (rest ops2) ops1' ops2'))
-       (retain-both? ops1 ops2)
-         (let [val1 (:val op1)
-               val2 (:val op2)]
-           (cond
-            (> val1 val2)
-              (let [ops1 (update-head ops1 (- val1 val2))
-                    [ops1' ops2'] (assoc-op retain [ops1' ops2'] val2)]
-                (recur ops1 (rest ops2) ops1' ops2'))
-            (= val1 val2)
-              (let [[ops1' ops2'] (assoc-op retain [ops1' ops2'] val2)]
-                (recur (rest ops1) (rest ops2) ops1' ops2'))
-            :else
-              (let [ops2 (update-head ops2 (- val2 val1))
-                    [ops1' ops2'] (assoc-op retain [ops1' ops2'] val1)]
-                (recur (rest ops1) ops2 ops1' ops2'))))))
+     (insert? ops2)
+     (let [[ops2' ops1'] (assoc-op insert [ops2' ops1'] (first ops2))]
+       (recur ops1 (rest ops2) ops1' ops2'))
+
+     (retain-both? ops1 ops2)
+     (let [val1 (:val (first ops1))
+           val2 (:val (first ops2))]
+       (cond
+        (> val1 val2)
+        (let [ops1 (update-head ops1 (- val1 val2))
+              [ops1' ops2'] (assoc-op retain [ops1' ops2'] val2)]
+          (recur ops1 (rest ops2) ops1' ops2'))
+        (= val1 val2)
+        (let [[ops1' ops2'] (assoc-op retain [ops1' ops2'] val2)]
+          (recur (rest ops1) (rest ops2) ops1' ops2'))
+        :else
+        (let [ops2 (update-head ops2 (- val2 val1))
+              [ops1' ops2'] (assoc-op retain [ops1' ops2'] val1)]
+          (recur (rest ops1) ops2 ops1' ops2')))))
 
     [ops1' ops2']))
 
