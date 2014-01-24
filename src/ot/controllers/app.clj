d3d0f582009aa0907aec67237de4d6ae2439d310
Jahfer Husain <jahferh@gmail.com>
2014-01-24 00:06:07 -0500

    Stream socket data to server-side chan

--- src/ot/controllers/app.clj
+++ src/ot/controllers/app.clj
@@ -1,5 +1,6 @@
 (ns ot.controllers.app
-  (:use [org.httpkit.server]))
+  (:use [org.httpkit.server])
+  (:require [clojure.core.async :refer [go put! <! chan]]))
 
 (defn generate-response [data & [status]]
   {:status (or status 200)
@@ -10,9 +11,18 @@
   (println (str "type: " type " val: " val))
   (generate-response {:hello type}))
 
+(def input (chan))
+
 (defn async-handler [req]
   (with-channel req ch
     (println "New WebSocket channel:" ch)
     (on-close ch (fn [status] (println "channel closed: " status)))
     (on-receive ch (fn [data]
+                     (put! input data)
                      (send! ch data)))))
+
+(go
+ (while true
+   (let [data (<! input)
+          parsed (clojure.edn/read-string data)]
+         (println "type:" (:type parsed) "val:" (:val parsed)))))
