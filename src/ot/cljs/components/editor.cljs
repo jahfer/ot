95d7b2ac720885c135b91c466ffbbc4dfc702c67
Jahfer Husain <jahferh@gmail.com>
2014-02-04 00:46:40 -0500

    Basic retain logic

--- src/ot/cljs/components/editor.cljs
+++ src/ot/cljs/components/editor.cljs
@@ -10,15 +10,35 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:use-macros [dommy.macros :only [node sel sel1]]))
 
+(defn cursor-position
+  "gets or sets the current cursor position"
+  ([]
+  (.-selectionStart (sel1 :#editor)))
+  ([new-pos]
+   (let [editor (sel1 :#editor)]
+     (set! (.-selectionStart editor) new-pos)
+     (set! (.-selectionEnd editor) new-pos))))
+
+(defn doc-length []
+  (.. (sel1 :#editor) -value -length))
+
+(defn gen-insert-op [key]
+  (let [cursor (js/parseInt (cursor-position) 10)
+        retain-before (transforms/op :ret cursor)
+        insert (transforms/op :ins key)
+        ; Kind of a hack because we don't know the new document length
+        ; Only works if we :ins by a single char at a time
+        chars-remaining (- (doc-length) cursor)
+        retain-after (transforms/op :ret chars-remaining)]
+    [retain-before insert retain-after]))
+
 (defn send-insert
   "Pushes the operation to the outbound buffer and updates
   the owned-ids with the new operation ID and the doc-id with
   a hash of the new document contents."
-  [owner {:keys [key id]}]
-  (let [ret-count (.-selectionStart (sel1 :#editor))
-        ops [(transforms/op :ret ret-count) (transforms/op :ins key)]
-        parent-id (om/get-state owner :doc-id)
-        data (pr-str {:id id :ops ops :parent-id parent-id})
+  [owner {:keys [operations id]}]
+  (let [parent-id (om/get-state owner :doc-id)
+        data (pr-str {:id id :ops operations :parent-id parent-id})
         owned-ids (om/get-state owner :owned-ids)]
     (go (put! queue/buffer data))
     (om/set-state! owner :owned-ids (conj owned-ids id))
@@ -33,10 +53,13 @@
   to the new result and dispatches an insert to the server."
   [e owner {:keys [text]}]
   (let [key (util/keyFromCode (.-which e))
-        new-text (str text key)
+        operations (gen-insert-op key)
+        new-text (doc/apply-ops operations text)
+        cached-cursor (cursor-position)
         id (js/md5 new-text)]
-    (send-insert owner {:key key :id id})
-    (om/set-state! owner :text new-text)))
+    (send-insert owner {:operations operations :id id})
+    (om/set-state! owner :text new-text)
+    (cursor-position cached-cursor)))
 
 (defn handle-incoming
   "Processes the inbound queue, applying the operations to
@@ -47,8 +70,12 @@
               data (cljs.reader.read-string (.-data raw))
               ops (:ops data)
               text (om/get-state owner :text)
-              applied (doc/apply-ops ops text)]
-          (om/set-state! owner :text applied)))))
+              cached-cursor (cursor-position)
+              new-text (doc/apply-ops ops text)]
+          (om/set-state! owner :text new-text)
+          (.log js/console "Setting cursor to" cached-cursor)
+          (cursor-position cached-cursor)))))
+
 
 (defn editor-view [app owner]
   (reify
